e8e0f2863a699d0892e180de531bc09c2a3a870e
asp24 <488588+asp24@users.noreply.github.com>
2026-03-29 18:45:00 +0200

    Extract argResolver.resolve switch cases into separate methods

--- ir/arg_resolver.go
+++ ir/arg_resolver.go
@@ -18,150 +18,162 @@ type argResolver struct {
 
 // resolve resolves a single constructor argument
 func (r *argResolver) resolve(container *Container, svcID string, idx int, arg di.Argument, paramType types.Type) (*Argument, error) {
-	irArg := &Argument{Type: paramType}
-
 	switch arg.Kind {
 	case di.ArgServiceRef:
-		dep, ok := container.Services[arg.Value]
-		if !ok {
-			return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: unknown service %q", svcID, idx, arg.Value)
-		}
-		irArg.Kind = ServiceRefArg
-		irArg.Service = dep
-
+		return r.resolveServiceRef(container, svcID, idx, arg, paramType)
 	case di.ArgInner:
 		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: @.inner should have been resolved by DecoratorPass", svcID, idx)
-
 	case di.ArgParam:
-		param, ok := container.Parameters[arg.Value]
-		if !ok {
-			// Parameter might be provided at runtime
-			irArg.Kind = ParamRefArg
-			irArg.Parameter = &Parameter{Name: arg.Value, Type: paramType}
-		} else {
-			if !types.AssignableTo(param.Type, paramType) {
-				return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: parameter %q type mismatch", svcID, idx, arg.Value)
-			}
-			irArg.Kind = ParamRefArg
-			irArg.Parameter = param
-		}
-
+		return r.resolveParam(container, svcID, idx, arg, paramType)
 	case di.ArgTagged:
-		tag, ok := container.tags[arg.Value]
-		if !ok {
-			// Create tag on-demand - infer ElementType from parameter
-			tag = &Tag{
-				Name:     arg.Value,
-				Services: []*Service{},
-			}
-			container.tags[arg.Value] = tag
-		}
-
-		// Infer or validate ElementType from parameter type
-		sliceType, ok := paramType.Underlying().(*types.Slice)
-		if !ok {
-			return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: tagged injection requires slice type, got %s", svcID, idx, paramType)
-		}
-		elemType := sliceType.Elem()
-
-		if tag.ElementType == nil {
-			// Infer ElementType from parameter
-			tag.ElementType = elemType
-		} else if !types.AssignableTo(tag.ElementType, elemType) {
-			// Validate consistency if ElementType was declared or inferred earlier
-			return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: tag %q element type mismatch: %s is not assignable to %s",
-				svcID, idx, arg.Value, tag.ElementType, elemType)
-		}
-
-		irArg.Kind = TaggedArg
-		irArg.Tag = tag
-
+		return r.resolveTagged(container, svcID, idx, arg, paramType)
 	case di.ArgSpread:
-		// Validate that parameter is variadic (represented as slice in go/types)
-		if _, ok := paramType.Underlying().(*types.Slice); !ok {
-			return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: !spread: can only be used with variadic parameters, got %s", svcID, idx, paramType)
-		}
-
-		// Parse inner argument string
-		innerKind, innerValue := yaml.ParseArgumentString(arg.Value)
-		innerArg := di.Argument{
-			Kind:  innerKind,
-			Value: innerValue,
-		}
-		// Preserve literal only if inner is also a literal
-		if innerKind == di.ArgLiteral {
-			innerArg.Literal = arg.Literal
-		}
-
-		// Resolve inner argument with the slice type (not element type)
-		// The inner expression should evaluate to []T
-		innerResolved, err := r.resolve(container, svcID, idx, innerArg, paramType)
-		if err != nil {
-			return nil, srcloc.WrapError(arg.SourceLoc, fmt.Sprintf("service %q arg[%d]: !spread", svcID, idx), err)
-		}
-
-		// Validate that inner resolved to a slice type
-		if _, ok := innerResolved.Type.Underlying().(*types.Slice); !ok {
-			return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: !spread: requires slice type, got %s", svcID, idx, innerResolved.Type)
-		}
-
-		irArg.Kind = SpreadArg
-		irArg.Inner = innerResolved
-
+		return r.resolveSpread(container, svcID, idx, arg, paramType)
 	case di.ArgFieldAccessService:
-		fa, err := r.resolveFieldAccessOnService(container, svcID, idx, arg, arg.Value)
-		if err != nil {
-			return nil, err
-		}
-		if !types.AssignableTo(fa.ResultType, paramType) {
-			return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: field access result type %s is not assignable to %s",
-				svcID, idx, fa.ResultType, paramType)
-		}
-		irArg.Kind = FieldAccessArg
-		irArg.FieldAccess = fa
-
+		return r.resolveFieldAccessServiceArg(container, svcID, idx, arg, paramType)
 	case di.ArgFieldAccessGo:
-		fa, err := r.resolveFieldAccessOnGoRef(svcID, idx, arg, arg.Value)
-		if err != nil {
-			return nil, err
-		}
-		if !types.AssignableTo(fa.ResultType, paramType) {
-			return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: field access result type %s is not assignable to %s",
-				svcID, idx, fa.ResultType, paramType)
-		}
-		irArg.Kind = FieldAccessArg
-		irArg.FieldAccess = fa
-
+		return r.resolveFieldAccessGoArg(svcID, idx, arg, paramType)
 	case di.ArgGoRef:
-		pkgPath, name, _, err := typeres.SplitQualifiedNameWithTypeParams(arg.Value)
-		if err != nil {
-			return nil, srcloc.WrapError(arg.SourceLoc, fmt.Sprintf("service %q arg[%d]: invalid go reference %q", svcID, idx, arg.Value), err)
-		}
-		obj, err := r.typeResolver.LookupVar(pkgPath, name)
-		if err != nil {
-			return nil, srcloc.WrapError(arg.SourceLoc, fmt.Sprintf("service %q arg[%d]", svcID, idx), err)
-		}
-		if !types.AssignableTo(obj.Type(), paramType) {
-			return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: go reference %q type %s is not assignable to %s",
-				svcID, idx, arg.Value, obj.Type(), paramType)
-		}
-		irArg.Kind = GoRefArg
-		irArg.GoRef = &GoRef{Object: obj}
+		return r.resolveGoRef(svcID, idx, arg, paramType)
+	case di.ArgLiteral:
+		return r.resolveLiteral(svcID, idx, arg, paramType)
+	default:
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: unknown argument kind %d", svcID, idx, arg.Kind)
+	}
+}
 
-	default: // Literal
-		// Validate that this is actually a literal argument
-		if arg.Kind != di.ArgLiteral {
-			return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: unknown argument kind %d", svcID, idx, arg.Kind)
+func (r *argResolver) resolveServiceRef(container *Container, svcID string, idx int, arg di.Argument, paramType types.Type) (*Argument, error) {
+	dep, ok := container.Services[arg.Value]
+	if !ok {
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: unknown service %q", svcID, idx, arg.Value)
+	}
+	return &Argument{Type: paramType, Kind: ServiceRefArg, Service: dep}, nil
+}
+
+func (r *argResolver) resolveParam(container *Container, svcID string, idx int, arg di.Argument, paramType types.Type) (*Argument, error) {
+	param, ok := container.Parameters[arg.Value]
+	if !ok {
+		// Parameter might be provided at runtime
+		return &Argument{
+			Type:      paramType,
+			Kind:      ParamRefArg,
+			Parameter: &Parameter{Name: arg.Value, Type: paramType},
+		}, nil
+	}
+	if !types.AssignableTo(param.Type, paramType) {
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: parameter %q type mismatch", svcID, idx, arg.Value)
+	}
+	return &Argument{Type: paramType, Kind: ParamRefArg, Parameter: param}, nil
+}
+
+func (r *argResolver) resolveTagged(container *Container, svcID string, idx int, arg di.Argument, paramType types.Type) (*Argument, error) {
+	tag, ok := container.tags[arg.Value]
+	if !ok {
+		// Create tag on-demand - infer ElementType from parameter
+		tag = &Tag{
+			Name:     arg.Value,
+			Services: []*Service{},
 		}
-		litVal, err := convertLiteral(arg.Literal, paramType)
-		if err != nil {
-			return nil, srcloc.WrapError(arg.SourceLoc, fmt.Sprintf("service %q arg[%d]", svcID, idx), err)
-		}
-		irArg.Kind = LiteralArg
-		irArg.Literal = litVal
+		container.tags[arg.Value] = tag
 	}
 
-	return irArg, nil
+	// Infer or validate ElementType from parameter type
+	sliceType, ok := paramType.Underlying().(*types.Slice)
+	if !ok {
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: tagged injection requires slice type, got %s", svcID, idx, paramType)
+	}
+	elemType := sliceType.Elem()
+
+	if tag.ElementType == nil {
+		// Infer ElementType from parameter
+		tag.ElementType = elemType
+	} else if !types.AssignableTo(tag.ElementType, elemType) {
+		// Validate consistency if ElementType was declared or inferred earlier
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: tag %q element type mismatch: %s is not assignable to %s",
+			svcID, idx, arg.Value, tag.ElementType, elemType)
+	}
+
+	return &Argument{Type: paramType, Kind: TaggedArg, Tag: tag}, nil
+}
+
+func (r *argResolver) resolveSpread(container *Container, svcID string, idx int, arg di.Argument, paramType types.Type) (*Argument, error) {
+	// Validate that parameter is variadic (represented as slice in go/types)
+	if _, ok := paramType.Underlying().(*types.Slice); !ok {
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: !spread: can only be used with variadic parameters, got %s", svcID, idx, paramType)
+	}
+
+	// Parse inner argument string
+	innerKind, innerValue := yaml.ParseArgumentString(arg.Value)
+	innerArg := di.Argument{
+		Kind:  innerKind,
+		Value: innerValue,
+	}
+	// Preserve literal only if inner is also a literal
+	if innerKind == di.ArgLiteral {
+		innerArg.Literal = arg.Literal
+	}
+
+	// Resolve inner argument with the slice type (not element type)
+	// The inner expression should evaluate to []T
+	innerResolved, err := r.resolve(container, svcID, idx, innerArg, paramType)
+	if err != nil {
+		return nil, srcloc.WrapError(arg.SourceLoc, fmt.Sprintf("service %q arg[%d]: !spread", svcID, idx), err)
+	}
+
+	// Validate that inner resolved to a slice type
+	if _, ok := innerResolved.Type.Underlying().(*types.Slice); !ok {
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: !spread: requires slice type, got %s", svcID, idx, innerResolved.Type)
+	}
+
+	return &Argument{Type: paramType, Kind: SpreadArg, Inner: innerResolved}, nil
+}
+
+func (r *argResolver) resolveFieldAccessServiceArg(container *Container, svcID string, idx int, arg di.Argument, paramType types.Type) (*Argument, error) {
+	fa, err := r.resolveFieldAccessOnService(container, svcID, idx, arg, arg.Value)
+	if err != nil {
+		return nil, err
+	}
+	if !types.AssignableTo(fa.ResultType, paramType) {
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: field access result type %s is not assignable to %s",
+			svcID, idx, fa.ResultType, paramType)
+	}
+	return &Argument{Type: paramType, Kind: FieldAccessArg, FieldAccess: fa}, nil
+}
+
+func (r *argResolver) resolveFieldAccessGoArg(svcID string, idx int, arg di.Argument, paramType types.Type) (*Argument, error) {
+	fa, err := r.resolveFieldAccessOnGoRef(svcID, idx, arg, arg.Value)
+	if err != nil {
+		return nil, err
+	}
+	if !types.AssignableTo(fa.ResultType, paramType) {
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: field access result type %s is not assignable to %s",
+			svcID, idx, fa.ResultType, paramType)
+	}
+	return &Argument{Type: paramType, Kind: FieldAccessArg, FieldAccess: fa}, nil
+}
+
+func (r *argResolver) resolveGoRef(svcID string, idx int, arg di.Argument, paramType types.Type) (*Argument, error) {
+	pkgPath, name, _, err := typeres.SplitQualifiedNameWithTypeParams(arg.Value)
+	if err != nil {
+		return nil, srcloc.WrapError(arg.SourceLoc, fmt.Sprintf("service %q arg[%d]: invalid go reference %q", svcID, idx, arg.Value), err)
+	}
+	obj, err := r.typeResolver.LookupVar(pkgPath, name)
+	if err != nil {
+		return nil, srcloc.WrapError(arg.SourceLoc, fmt.Sprintf("service %q arg[%d]", svcID, idx), err)
+	}
+	if !types.AssignableTo(obj.Type(), paramType) {
+		return nil, srcloc.Errorf(arg.SourceLoc, "service %q arg[%d]: go reference %q type %s is not assignable to %s",
+			svcID, idx, arg.Value, obj.Type(), paramType)
+	}
+	return &Argument{Type: paramType, Kind: GoRefArg, GoRef: &GoRef{Object: obj}}, nil
+}
+
+func (r *argResolver) resolveLiteral(svcID string, idx int, arg di.Argument, paramType types.Type) (*Argument, error) {
+	litVal, err := convertLiteral(arg.Literal, paramType)
+	if err != nil {
+		return nil, srcloc.WrapError(arg.SourceLoc, fmt.Sprintf("service %q arg[%d]", svcID, idx), err)
+	}
+	return &Argument{Type: paramType, Kind: LiteralArg, Literal: litVal}, nil
 }
 
 // resolveFieldAccessOnService resolves !field:@service.Field.Chain.
