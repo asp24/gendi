91a1d4e13ddc9d15d17aadcf7d792045fd30c912
asp24 <488588+asp24@users.noreply.github.com>
2026-07-20 23:02:15 +0200

    Run TestWorkflow tests in parallel

--- integration/integration_test.go
+++ integration/integration_test.go
@@ -183,6 +183,7 @@ func TestWorkflow(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			runEmbeddedTest(t, tt.name, tt.expectedOutput, tt.wantCompileErr, tt.wantRuntimeErr)
 		})
 	}
