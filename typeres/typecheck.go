d6639a26903b318d48e5d5cf96d033394d8f3c2f
asp24 <488588+asp24@users.noreply.github.com>
2026-07-21 16:08:24 +0200

    Drop unused code

--- typeres/typecheck.go
+++ typeres/typecheck.go
@@ -33,23 +33,3 @@ func IsTime(t types.Type) bool {
 	pkg := obj.Pkg()
 	return pkg != nil && pkg.Path() == "time"
 }
-
-// IsString returns true if t's underlying type is the built-in string type.
-func IsString(t types.Type) bool {
-	return types.Identical(t.Underlying(), types.Typ[types.String])
-}
-
-// IsInt returns true if t's underlying type is the built-in int type.
-func IsInt(t types.Type) bool {
-	return types.Identical(t.Underlying(), types.Typ[types.Int])
-}
-
-// IsBool returns true if t's underlying type is the built-in bool type.
-func IsBool(t types.Type) bool {
-	return types.Identical(t.Underlying(), types.Typ[types.Bool])
-}
-
-// IsFloat64 returns true if t's underlying type is the built-in float64 type.
-func IsFloat64(t types.Type) bool {
-	return types.Identical(t.Underlying(), types.Typ[types.Float64])
-}
