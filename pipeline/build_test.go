2d9a83ae3a42edcb197d121532361257550cad1e
asp24 <488588+asp24@users.noreply.github.com>
2026-07-23 13:15:06 +0200

    Consolidate collectPackagePaths tests into table

--- pipeline/build_test.go
+++ pipeline/build_test.go
@@ -6,88 +6,86 @@ import (
 	di "github.com/gendi-org/gendi"
 )
 
-func TestCollectPackagePathsWithTypeArgs(t *testing.T) {
-	cfg := &di.Config{
-		Services: map[string]di.Service{
-			"events": {
-				Constructor: di.Constructor{
-					Func: "github.com/utils.NewChan[github.com/events.Event]",
+func TestCollectPackagePaths(t *testing.T) {
+	tests := []struct {
+		name     string
+		services map[string]di.Service
+		want     map[string]bool
+	}{
+		{
+			name: "type args from other packages",
+			services: map[string]di.Service{
+				"events": {
+					Constructor: di.Constructor{
+						Func: "github.com/utils.NewChan[github.com/events.Event]",
+					},
+				},
+				"map_svc": {
+					Constructor: di.Constructor{
+						Func: "github.com/utils.NewMap[string, github.com/models.User]",
+					},
+				},
+				"nested": {
+					Constructor: di.Constructor{
+						Func: "github.com/utils.NewSlice[chan github.com/msgs.Message]",
+					},
 				},
 			},
-			"map_svc": {
-				Constructor: di.Constructor{
-					Func: "github.com/utils.NewMap[string, github.com/models.User]",
+			want: map[string]bool{
+				"github.com/utils":  true,
+				"github.com/events": true,
+				"github.com/models": true,
+				"github.com/msgs":   true,
+			},
+		},
+		{
+			name: "generic types in type field",
+			services: map[string]di.Service{
+				"pool": {
+					// Generic type in Type field with type argument from different package
+					Type: "*github.com/containers.Pool[github.com/models.User]",
+					Constructor: di.Constructor{
+						Func: "github.com/containers.NewPool[github.com/models.User]",
+					},
+				},
+				"nested": {
+					// Nested generic type
+					Type: "github.com/outer.Box[github.com/inner.Item[github.com/deep.Value]]",
+					Constructor: di.Constructor{
+						Func: "github.com/outer.NewBox[github.com/inner.Item[github.com/deep.Value]]",
+					},
 				},
 			},
-			"nested": {
-				Constructor: di.Constructor{
-					Func: "github.com/utils.NewSlice[chan github.com/msgs.Message]",
-				},
+			want: map[string]bool{
+				"github.com/containers": true,
+				"github.com/models":     true,
+				"github.com/outer":      true,
+				"github.com/inner":      true,
+				"github.com/deep":       true,
 			},
 		},
 	}
 
-	refreshPackages(cfg)
-	paths, _ := collectPackagePaths(cfg)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg := &di.Config{Services: tt.services}
 
-	expected := map[string]bool{
-		"github.com/utils":  true,
-		"github.com/events": true,
-		"github.com/models": true,
-		"github.com/msgs":   true,
-	}
+			refreshPackages(cfg)
+			paths, _ := collectPackagePaths(cfg)
 
-	for _, p := range paths {
-		if !expected[p] {
-			t.Errorf("unexpected package: %s", p)
-		}
-		delete(expected, p)
-	}
-
-	for p := range expected {
-		t.Errorf("missing expected package: %s", p)
-	}
-}
-
-func TestCollectPackagePathsWithGenericTypes(t *testing.T) {
-	cfg := &di.Config{
-		Services: map[string]di.Service{
-			"pool": {
-				// Generic type in Type field with type argument from different package
-				Type: "*github.com/containers.Pool[github.com/models.User]",
-				Constructor: di.Constructor{
-					Func: "github.com/containers.NewPool[github.com/models.User]",
-				},
-			},
-			"nested": {
-				// Nested generic type
-				Type: "github.com/outer.Box[github.com/inner.Item[github.com/deep.Value]]",
-				Constructor: di.Constructor{
-					Func: "github.com/outer.NewBox[github.com/inner.Item[github.com/deep.Value]]",
-				},
-			},
-		},
-	}
-
-	refreshPackages(cfg)
-	paths, _ := collectPackagePaths(cfg)
-
-	expected := map[string]bool{
-		"github.com/containers": true,
-		"github.com/models":     true,
-		"github.com/outer":      true,
-		"github.com/inner":      true,
-		"github.com/deep":       true,
-	}
-
-	for _, p := range paths {
-		if !expected[p] {
-			t.Errorf("unexpected package: %s", p)
-		}
-		delete(expected, p)
-	}
-
-	for p := range expected {
-		t.Errorf("missing expected package: %s", p)
+			expected := make(map[string]bool, len(tt.want))
+			for p := range tt.want {
+				expected[p] = true
+			}
+			for _, p := range paths {
+				if !expected[p] {
+					t.Errorf("unexpected package: %s", p)
+				}
+				delete(expected, p)
+			}
+			for p := range expected {
+				t.Errorf("missing expected package: %s", p)
+			}
+		})
 	}
 }
